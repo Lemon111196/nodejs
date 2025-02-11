71974a83e5eadf594aaea9734f06272e9cab397b
ngocdt <ngocdt1@runsystem.net>
2025-02-11 09:16:38 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const path = require('path');
+
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/view'));
+
+app.get('/', (req, res) => {
+        res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
